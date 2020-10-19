5d1f71dc9b90984a04f78b556c7897c94aef7e70
Ansh <anshsarin63@gmail.com>
2020-10-19 13:36:54 +0530

    new project

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,15 @@
+const Menu = require('../../models/menu');
+
+function homeController() {
+    return {
+        index(req, res) {
+            
+            Menu.find().then(function (pizza) {
+                console.log(pizza);
+                res.render("home",{pizzas:pizza});
+            })
+        }
+    }
+}
+
+module.exports = homeController;
--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const app = express();
 const ejs = require("ejs");
 const path = require("path");
 const expressLayout = require("express-ejs-layouts");
+const mongoose = require('mongoose');
 
 
 //assets
 app.use(express.static("public"));
+
+//database-mongoDB
+mongoose.connect(
+  "mongodb+srv://admin-ansh:anshsarin00@cluster0.2l6ki.mongodb.net/pizza",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Database connected");
+}).catch(err => {
+    console.log("Connection failed");
+})
 
 
 //set views
@@ -14,19 +30,8 @@ app.use(expressLayout);
 app.set("views", path.join(__dirname + "/resources/views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home");
-})
-app.get("/cart", (req, res) => {
-    res.render("customer/cart");
-})
-app.get("/login", (req, res) => {
-    res.render("auth/login");
-})
-app.get("/register", (req, res) => {
-  res.render("auth/register");
-});
-
+//routes
+require('./routes/web')(app);
 
 
 
